8488f97f47de9f29cfe1d0f757242cf7e4e20b3f
angelnogueras <angelnogueras.git@gmail.com>
2021-08-17 17:48:41 +0200

    added session to save and validate nonce

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ const fs = require('fs');
 const https = require('https');
 const helmet = require('helmet');
 const csp = require('helmet-csp');
+const session = require('express-session');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//// MiddleWares ////
+// Security Middleware
 app.use(helmet());
 app.use(csp({
   useDefaults: true,
@@ -22,10 +25,23 @@ app.use(csp({
 }));
 app.disable('x-powered-by');
 
+// Session Middleware
+app.use(session({
+  secret: process.env.SECRET_SIGN_SESSION_COOKIE,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { 
+    maxAge: 30 * 60 * 1000, // 30min (in ms.)
+    secure: true,
+    sameSite: 'lax',
+  },
+}));
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+//// MiddleWares ////
 
 // Routes
 app.use(require('./lib/routes/index'));
